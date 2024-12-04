f81313cd0e4b46ec82f7b9da769390eed0f10fc8
Zac-Azev <133678324+Zac-Azev@users.noreply.github.com>
2024-12-04 10:03:34 -0300

    Update Admin.js

--- src/model/Admin.js
+++ src/model/Admin.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const AdmSchema extends AbstractUser = new mongoose.Schema({
-  isAdmin: boolean
+  isAdmin: Boolean
 });
 
 module.exports = mongoose.model('Adm', AdmSchema);
