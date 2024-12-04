b95a1c07f18f44e7bf6e75a84fd08b910066a2aa
Zac-Azev <133678324+Zac-Azev@users.noreply.github.com>
2024-12-04 09:59:00 -0300

    Create Fnc.js

---
+++ src/model/Fnc.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const abstract = require('mongoose');
+
+const FncSchema extends AbstractUser = new mongoose.Schema({});
+
+module.exports = mongoose.model('Fnc', FncSchema);
